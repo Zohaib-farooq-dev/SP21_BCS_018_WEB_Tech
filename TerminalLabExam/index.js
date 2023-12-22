73972732257a4d8f877f9c97dc2f3e5afba5fcf5
Zohaib-farooq-dev <zohaibx595@gmail.com>
2023-12-22 15:39:34 +0500

    final lab commit

---
+++ TerminalLabExam/index.js
@@ -0,0 +1,169 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const server = express();
+let Students = require("./Model/students");
+var expressLayouts = require("express-ejs-layouts");
+const multer = require("multer")
+const path = require("path")
+let checkSessionAuth = require("./middlewares/checkSessionAuth");
+
+//Use For Body Parser
+server.use(express.json());
+
+//getting data from url
+server.use(express.urlencoded({extended: false}));
+server.use(cookieParser());
+
+server.use(
+  session({
+    secret: "Dont copy code work hard",
+    cookie: { maxAge: 600000 },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//Define Static Folder
+server.use(express.static("public"));
+
+
+server.set("view engine", "ejs");
+server.use(expressLayouts);
+const PORT = 5000;
+server.use(require("./middlewares/siteSettings"));
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    }, filename: (req, file, cb) => {
+   
+        var temp=Date.now() + path.extname(file.originalname)
+        cb(null,temp )
+        req.body={...req.body,image:temp}
+    }
+
+})
+
+const upload = multer({ storage })
+
+
+server.get("/",async(req, res, next) => {
+    let students = await Students.find();
+ 
+  res.render("home",{students});
+});
+server.get("/add",checkSessionAuth,  (req, res, next) => {
+  res.render("add");
+});
+server.get("/contactus", (req, res, next) => {
+  res.render("contactus");
+});
+server.get("/about", (req, res, next) => {
+    res.render("about");
+  });
+
+// Handling form submission for the calculator
+server.post('/calculate', (req, res) => {
+  const { operand1, operand2, operator } = req.body;
+  let result;
+  switch (operator) {
+    case '+':
+      result = parseFloat(operand1) + parseFloat(operand2);
+      break;
+    case '-':
+      result = parseFloat(operand1) - parseFloat(operand2);
+      break;
+    case '*':
+      result = parseFloat(operand1) * parseFloat(operand2);
+      break;
+    case '/':
+      result = parseFloat(operand1) / parseFloat(operand2);
+      break;
+    default:
+      result = 'Invalid operation';
+  }
+
+  // Store operands, operator, and result in session array
+  const calculation = { operand1, operator, operand2, result };
+  req.session.results = req.session.results || []; // Initialize session array if not exists
+  req.session.results.push(calculation); // Store the calculation in the session array
+  
+  res.redirect('/calculator'); // Redirect back to the calculator page
+});
+  // A route to render the calculator form
+server.get('/calculator', (req, res) => {
+  const results = req.session.results || []; // Retrieve session-stored results
+  res.render('calculator', { results }); // Render the EJS file and pass session results
+});
+
+
+  server.get("/view:id",checkSessionAuth,  async(req, res, next) => {
+   
+    let students = await Students.find({_id:(req.params.id).split(":")[1]});
+    
+    res.render("view",{data:students[0]});
+  });
+
+  server.get("/edit:id",checkSessionAuth,  async(req, res, next) => {
+    
+    let students = await Students.find({_id:(req.params.id).split(":")[1]});
+   
+    res.render("edit",{data:students[0]});
+  });
+  server.post("/update",checkSessionAuth,upload.single('image') , async(req, res, next) => {
+   
+    let students = await Students.find({rollnumber:req.body.rollnumber});
+    let stude = await Students.updateOne({_id:students[0]._id},req.body);
+    
+    res.redirect('/')
+  });
+
+
+  //Students
+  server.post('/addStudents',upload.single('image'),async(req,res,next)=>{
+    try{
+      let student = new Students(req.body);
+      await student.save();
+      res.redirect('/')
+    }catch(e){
+      console.log("error",e.code)
+      if(e.code==11000){
+        req.flash("danger", "User with this email is already present");
+        res.redirect('/add')
+      }
+    }
+  })
+
+  server.get("/delete:id",checkSessionAuth,  async(req, res, next) => {
+    let students = await Students.findByIdAndDelete({_id:(req.params.id).split(":")[1]});
+    res.redirect('/')
+  });
+
+  server.get('/login',(req,res)=>{
+    res.render("login");
+  })
+  server.get('/register',(req,res)=>{
+    res.render("register");
+  })
+  
+  server.use("/api/auth", require("./routes/auth"));
+
+  server.use((req, res, next) => {
+    res.render("notfound");
+  });
+
+server.listen(PORT, () => {
+  console.log(`Server is running on  port ${PORT}`);
+});
+
+const MONGODBURL =
+  "mongodb://localhost:27017/";
+mongoose
+  .connect(MONGODBURL, { useNewUrlParser: true })
+  .then(() => console.log("Connected to Mongo ...."))
+  .catch((error) => console.log(error.message));
+
+ 
